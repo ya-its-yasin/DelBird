e7726c120a51ec9c207d8e034fc7c77433236624
Ya sin <66527121+ya-its-yasin@users.noreply.github.com>
2020-11-19 18:27:15 +0530

    updated userDAO

--- src/aurora/common/dao/DelUserDAO.java
+++ src/aurora/common/dao/DelUserDAO.java
@@ -1,5 +1,58 @@
 package aurora.common.dao;
 
-public class DelUserDAO {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
+import aurora.common.ANavigationDao;
+import aurora.common.IMasterDao;
+
+@SuppressWarnings("unchecked")
+@Repository("delUserDAO")
+public class DelUserDAO  extends ANavigationDao implements IMasterDao  {
+	
+	@Autowired @Qualifier("hibernateTemplate")
+	private HibernateTemplate hibernateTemplate;
+
+	@Override
+	public Object add(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object update(Object obj) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object delete(long key) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getObjectByKey(long key) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<?> getAll(String lang) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<?> getList(String lang) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 
 }
